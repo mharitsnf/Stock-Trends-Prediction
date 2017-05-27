6706e00a5cc3e8036dcf7dd9181dd2ab39026c24
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2017-05-27 12:18:53 +0700

    adding new script

--- Script/Mandiri.R
+++ Script/Mandiri.R
@@ -37,3 +37,25 @@ yearlyMandiri <- function() {
   return(newDf)
 }
 mandiri_year <- yearlyMandiri()
+
+#Data normalization
+norm_mandiri_year <- as.data.frame(scale(mandiri_year[2:7]))
+
+#Making models and doing regression
+model1 <- lm(Mean~Low+Volume, norm_mandiri_year)
+summary(model1)
+set.seed(666)
+n <- length(norm_mandiri_year$Mean)
+n1 <- 10
+n2 <- n-n1
+train <- sample(1:n,n1)
+pred <- predict(model1, newdat = norm_mandiri_year[-train,])
+obs <- norm_mandiri_year$Mean[-train]
+diff <- obs-pred
+percdiff <- abs(diff)/obs
+me <- mean(diff)
+rmse <- sqrt(sum(diff**2)/n2)
+mape <- 100*(mean(percdiff))
+me
+rmse
+mape
