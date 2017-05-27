1d52d32c8896d3b7946c46d38bdb625eeaa25fba
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2017-05-27 10:51:27 +0700

    remove script, adding comments, creating yearly data

--- Script/Mandiri.R
+++ Script/Mandiri.R
@@ -1,5 +1,9 @@
+#Before doing this, I've do some wrangling: sorting the data
+#based on year
 setwd("~/DSAFinalProject/")
 mandiri <- read.csv("Data/bmri_2005.csv")
+
+#Preprocessing data
 mandiri$Volume <- as.numeric(mandiri$Volume)
 mandiri <- mandiri[,-1]
 mandiri$Mean <- rowMeans(mandiri[,5:6])
@@ -11,3 +15,25 @@ mandiri$High <- as.numeric(mandiri$High)
 mandiri$Low <- as.numeric(mandiri$Low)
 mandiri$Close <- as.numeric(mandiri$Close)
 str(mandiri)
+
+#Install sql package for ease querying
+install.packages('sqldf')
+library(sqldf)
+
+#Function for creating yearly observations (Aggregating)
+yearlyMandiri <- function() {
+  newDf <- data.frame(Year = factor(), Open = numeric(), High = numeric(), Low = numeric(), Close = numeric(), Volume = numeric(), Mean = numeric())
+  for (year in levels(mandiri$Year)) {
+    tmpdata <- sqldf(strwrap(sprintf("SELECT * FROM mandiri WHERE Year = %s", year)))
+    tmpOpen <- mean(tmpdata$Open)
+    tmpHigh <- mean(tmpdata$High)
+    tmpLow <- mean(tmpdata$Low)
+    tmpClose <- mean(tmpdata$Close)
+    tmpVolume <- mean(tmpdata$Volume)
+    tmpMean <- mean(tmpdata$Mean)
+    insertdf <- data.frame(Year = year, Open = tmpOpen, High = tmpHigh, Low = tmpLow, Close = tmpClose, Volume = tmpVolume, Mean = tmpMean)
+    newDf <- rbind(newDf, insertdf)
+  }
+  return(newDf)
+}
+mandiri_year <- yearlyMandiri()
