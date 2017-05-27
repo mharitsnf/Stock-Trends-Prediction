d9a91434ad61b40296a8d282e58418a6b6731914
Mohamad Harits Nur Fauzan <mharitsnf@gmail.com>
2017-05-27 20:52:02 +0700

    add yearly script

--- Script/Mandiri/yearly_mandiri_prediction.R
+++ Script/Mandiri/yearly_mandiri_prediction.R
@@ -16,21 +16,21 @@ mandiri$Volume <- as.numeric(mandiri$Volume)
 str(mandiri)
 
 #Create mandiri monthly observation table
-monthly_mandiri <- monthlyMandiri()
+yearly_mandiri <- yearlyMandiri()
 
 #Normalize the table
-norm_monthly_mandiri <- as.data.frame(scale(monthly_mandiri[,3:8]))
+norm_yearly_mandiri <- as.data.frame(scale(yearly_mandiri[,2:7]))
 
 #K-fold cross validation for checking errors
 install.packages("cvTools", "e1071")
 library(cvTools)
 library(e1071)
-k <- 5
-folds <- cvFolds(NROW(norm_monthly_mandiri), K=k)
+k <- 3
+folds <- cvFolds(NROW(norm_yearly_mandiri), K=k)
 errors <- data.frame(me = double(), rmse = double(), mape = double()) #dataframe for collecting the errors
 for(i in 1:k){
-  data_train <- norm_monthly_mandiri[folds$subsets[folds$which != i], ]
-  data_test <- norm_monthly_mandiri[folds$subsets[folds$which == i], ]
+  data_train <- norm_yearly_mandiri[folds$subsets[folds$which != i], ]
+  data_test <- norm_yearly_mandiri[folds$subsets[folds$which == i], ]
   model <- lm(Mean ~ Volume + Low, data_train) #making model from training
   pred <- predict(model, newdat=data_test) #making model from testing
   obs <- data_test$Mean
@@ -44,24 +44,24 @@ for(i in 1:k){
 }
 
 #Add new row to norm_mandiri
-last_open <- norm_monthly_mandiri$Open[nrow(norm_monthly_mandiri)]
-last_high <- norm_monthly_mandiri$High[nrow(norm_monthly_mandiri)]
-last_low <- norm_monthly_mandiri$Low[nrow(norm_monthly_mandiri)]
-last_close <- norm_monthly_mandiri$Close[nrow(norm_monthly_mandiri)]
-last_volume <- norm_monthly_mandiri$Volume[nrow(norm_monthly_mandiri)]
-last_mean <- norm_monthly_mandiri$Mean[nrow(norm_monthly_mandiri)]
-norm_monthly_mandiri[nrow(norm_monthly_mandiri) + 1, ] <- c(last_open, last_high, last_low, last_close, last_volume, last_mean)
+last_open <- norm_yearly_mandiri$Open[nrow(norm_yearly_mandiri)]
+last_high <- norm_yearly_mandiri$High[nrow(norm_yearly_mandiri)]
+last_low <- norm_yearly_mandiri$Low[nrow(norm_yearly_mandiri)]
+last_close <- norm_yearly_mandiri$Close[nrow(norm_yearly_mandiri)]
+last_volume <- norm_yearly_mandiri$Volume[nrow(norm_yearly_mandiri)]
+last_mean <- norm_yearly_mandiri$Mean[nrow(norm_yearly_mandiri)]
+norm_yearly_mandiri[nrow(norm_yearly_mandiri) + 1, ] <- c(last_open, last_high, last_low, last_close, last_volume, last_mean)
 
 #Predicting
-pred_train <- norm_monthly_mandiri[1:nrow(norm_monthly_mandiri)-1, ]
-pred_test <- norm_monthly_mandiri[nrow(norm_monthly_mandiri), ]
+pred_train <- norm_yearly_mandiri[1:nrow(norm_yearly_mandiri)-1, ]
+pred_test <- norm_yearly_mandiri[nrow(norm_yearly_mandiri), ]
 pred_model <- lm(Mean ~ Volume + Low, pred_train)
 res <- predict(pred_model, newdat=pred_test)
-norm_monthly_mandiri$Mean[nrow(norm_monthly_mandiri)] <- res
+norm_yearly_mandiri$Mean[nrow(norm_yearly_mandiri)] <- res
 
 #Visualize the data
-norm_monthly_mandiri$ID <- seq.int(nrow(norm_monthly_mandiri))
-attach(norm_monthly_mandiri)
-plot(ID[nrow(norm_monthly_mandiri)-10:nrow(norm_monthly_mandiri)], Mean[nrow(norm_monthly_mandiri)-10:nrow(norm_monthly_mandiri)], main="Scatterplot Example", xlab="Time ", ylab="Mean ", pch=20)
+norm_yearly_mandiri$ID <- seq.int(nrow(norm_yearly_mandiri))
+attach(norm_yearly_mandiri)
+plot(ID, Mean, main="Scatterplot Example", xlab="Time ", ylab="Mean ", pch=20)
 abline(lm(Mean~ID), col="red")
-detach(norm_monthly_mandiri)
+detach(norm_yearly_mandiri)
